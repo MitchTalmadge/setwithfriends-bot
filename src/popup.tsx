7b486ef8035e9722fdfd9c13153cb012ad60d351
Mitch Talmadge <mitch@mitchtalmadge.com>
2021-05-07 00:26:33 -0600

    Initial Commit

---
+++ src/popup.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const Popup = () => {
+
+
+    return (
+        <div>
+            <h1>SetWithFriends Bot</h1>
+            <hr />
+        </div>
+    );
+};
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Popup />
+    </React.StrictMode>,
+    document.getElementById("app")
+);
